374c936ab39eaa37efb2f3ef0733a8bc56499443
Henry Lee <henryleeworld@gmail.com>
2020-10-12 00:03:54 +0800

    feat:初始化

---
+++ resources/lang/vendor/laravel-nist-password-rules/zh_TW/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'can-not-contain-word'              => ':attribute 不能包含單字 \':word\'.',
+    'can-not-be-similar-to-word'        => ':attribute 不能類似於單字 \':word\'.',
+    'found-in-data-breach'              => ':attribute 在第三方數據洩露中被發現，無法使用。',
+    'can-not-be-dictionary-word'        => ':attribute 不能是字典單字。',
+    'can-not-be-repetitive-characters'  => ':attribute 不能為重複字元。',
+    'can-not-be-sequential-characters'  => ':attribute 不能是連續字元。',
+];
